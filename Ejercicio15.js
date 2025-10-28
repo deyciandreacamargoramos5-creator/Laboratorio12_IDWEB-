86ae894838df74520ea70c6a9330b736b387afa3
deyciandreacamargoramos5-creator <dcamargor@unsa.edu.pe>
2025-10-27 23:06:59 -0500

    Subiendo quinceavo commit: Ejercicio 15

---
+++ Ejercicio15.js
@@ -0,0 +1,7 @@
+function saludo(nombre = "Amigo") {
+  return "Hola" + nombre;
+}
+console.log(saludo("Juan"));     
+console.log(saludo("María"));    
+console.log(saludo());           
+console.log(saludo(""));         

6b3766069b38f5df35c6929c451073f2c57c878c
deyciandreacamargoramos5-creator <dcamargor@unsa.edu.pe>
2025-10-27 21:20:44 -0500

    Subiendo primer commit: Ejercicio 1

---
+++ Ejercicio1.js
@@ -0,0 +1,8 @@
+function esPar(){
+    let num = parseFloat(prompt("Ingresa un numero"));
+    if (num % 2 === 0){
+        console.log("El número es par")
+    }else{
+        console.log("El número es impar")
+    }
+}

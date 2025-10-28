cb8ac744d47860822a51f08366b7799d8feedaec
deyciandreacamargoramos5-creator <dcamargor@unsa.edu.pe>
2025-10-27 21:32:32 -0500

    Subiendo segundo commit: Ejercicio 2

---
+++ Ejercicio2.js
@@ -0,0 +1,7 @@
+function AreaRectangulo(){
+    let base = parseFloat(prompt("Ingresa la base del rectangulo"));
+    let altura = parseFloat(prompt("Ingresa la altura del rectangulo"));
+    let Area =  base * altura;
+    console.log("El area del rectangulo cuando la base es: "+base +" y la altura es: " +altura+" es: " +  Area);
+    
+}
